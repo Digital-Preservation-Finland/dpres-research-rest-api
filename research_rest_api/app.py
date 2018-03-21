9bea4acfa71259bd3ac6854289505a59d5a8e957
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-03-21 17:26:04 +0200

    TPASPKT-87 Set HTTP response status codes

--- research_rest_api/app.py
+++ research_rest_api/app.py
@@ -14,28 +14,36 @@ def create_app():
     """
     app = flask.Flask(__name__)
 
-    @app.route('/validate/<dataset_id>')
-    def validate(dataset_id):
-        """Dummy function that returns hard-coded json-message
 
-        :returns: Dymmy json message
+    @app.route('/dataset/<dataset_id>/validate')
+    def validate(dataset_id):
+        """Validates dataset.
+
+        :returns: HTTP Response
         """
-        # TODO: Implement dataset validation triggering
-        return flask.jsonify({'dataset_id': dataset_id, 'status': 'valid'})
+        response = flask.jsonify({'dataset_id': dataset_id,
+                                  'status': 'validated',
+                                  'validation_result': True})
+        response.status_code = 200
+        return response
+
 
     @app.route('/dataset/<dataset_id>/preserve')
     def preserve(dataset_id):
         """Trigger packaging of dataset.
 
-        :returns: json message
+        :returns: HTTP Response
         """
 
         # Trigger dataset preservation using function provided by
         # siptools_research package.
         preserve_dataset(dataset_id, '/etc/siptools_research.conf')
 
-        # TODO: What should this response be?
-        return flask.jsonify({'dataset_id': dataset_id, 'status': 'packaging'})
+        response = flask.jsonify({'dataset_id': dataset_id,
+                                  'status': 'packaging'})
+        response.status_code = 202
+
+        return response
 
 
     @app.route('/')
@@ -46,7 +54,10 @@ def create_app():
 
     @app.errorhandler(404)
     def page_not_found(error):
-        """JSON response handler for the 404 - Not found errors"""
+        """JSON response handler for the 404 - Not found errors
+
+        :returns: HTTP Response
+        """
 
         response = flask.jsonify({"code": 404, "error": str(error)})
         response.status_code = 404
@@ -56,7 +67,10 @@ def create_app():
 
     @app.errorhandler(400)
     def bad_request(error):
-        """JSON response handler for the 400 - Bad request errors"""
+        """JSON response handler for the 400 - Bad request errors
+
+        :returns: HTTP Response
+        """
 
         response = flask.jsonify({"code": 400, "error": str(error)})
         response.status_code = 400
