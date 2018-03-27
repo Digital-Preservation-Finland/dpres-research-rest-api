82964e2f3fa8896ad305ff488dd7300146f4c8ab
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2018-03-27 16:32:00 +0300

    TPASPKT-88 Add test for validate metadata API call

--- tests/app_test.py
+++ tests/app_test.py
@@ -35,3 +35,22 @@ def test_dataset_preserve():
         response = client.get('/dataset/1/preserve')
 
         assert response.status_code == 202
+
+
+def test_dataset_validate():
+    """Test the validate method.
+
+    :returns: None
+    """
+
+    # Create app and change the default config file path
+    app = create_app()
+    app.config.update(
+        SIPTOOLS_RESEARCH_CONF='tests/data/siptools_research.conf'
+    )
+
+    # Test the response
+    with app.test_client() as client:
+        response = client.get('/dataset/1/validate')
+
+        assert response.status_code == 200
