71a6fdf85caa30be8d2f7f65eb0bdd029f55141e
Janne Pulkkinen <janne.pulkkinen@csc.fi>
2025-03-31 12:54:13 +0000

    TPASPKT-1406 Re-enable upload-rest-api E2E tests
    upload-rest-api is now available for Metax V3, so add the tests back
    with the necessary changes. Most importantly we don't need to deal with
    directory identifiers which makes the test setup a bit shorter.
    
    A new dataset and directory for test files is created on Metax and
    upload-rest-api respectively for each test run. This also makes
    investigating failed test runs easier.

--- tests/e2e/system_test.py
+++ tests/e2e/system_test.py
@@ -17,7 +17,10 @@ System environment setup
 IDA service is mocked. Metax is installed and deployed on the same
 machine via ansible-fairdata-pas.
 """
+import base64
 import configparser
+import copy
+import datetime
 import json
 import logging
 import pathlib
@@ -25,26 +28,34 @@ import subprocess
 
 import pytest
 import requests
-from metax_access import (
-    DS_STATE_ACCEPTED_TO_DIGITAL_PRESERVATION,
-    DS_STATE_GENERATING_METADATA,
-    DS_STATE_IN_DIGITAL_PRESERVATION,
-    DS_STATE_METADATA_CONFIRMED,
-    DS_STATE_NONE,
-    DS_STATE_REJECTED_IN_DIGITAL_PRESERVATION_SERVICE,
-    DS_STATE_TECHNICAL_METADATA_GENERATED,
-    DS_STATE_VALIDATING_METADATA,
-    Metax,
-)
+import tusclient.client
+import tusclient.exceptions
+from metax_access import (DS_STATE_ACCEPTED_TO_DIGITAL_PRESERVATION,
+                          DS_STATE_GENERATING_METADATA,
+                          DS_STATE_IN_DIGITAL_PRESERVATION,
+                          DS_STATE_METADATA_CONFIRMED, DS_STATE_NONE,
+                          DS_STATE_REJECTED_IN_DIGITAL_PRESERVATION_SERVICE,
+                          DS_STATE_TECHNICAL_METADATA_GENERATED,
+                          DS_STATE_VALIDATING_METADATA, Metax)
 from pymongo import MongoClient
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-from tests.utils import wait_for
 from upload_rest_api.config import CONFIG
+from upload_rest_api.models.project import Project
+from upload_rest_api.models.user import User
+
+from tests.utils import wait_for
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+
+def _get_upload_url():
+    return json.loads(
+        pathlib.Path("/usr/share/nginx/html/fddps-frontend/config.json")
+        .read_text()
+    )["uploadApiUrl"]
 
 
 def _get_admin_url():
@@ -54,6 +65,7 @@ def _get_admin_url():
 
 
 ADMIN_API_URL = f"{_get_admin_url()}/secure/api/1.0"
+UPLOAD_API_URL = f"{_get_upload_url()}/v1"
 REQUESTS_SESSION = requests.Session()
 REQUESTS_SESSION.verify = False
 
@@ -64,22 +76,7 @@ DATASET = {
     "description": {
         "en": "Test description"
     },
-    "fileset": {
-        # These files are added to Metax in ansible playbook
-        "file_actions": [
-            {
-                "action": "add",
-                "storage_identifier": "dfea5aa8-c9bc-4333-8eab-0b17ee6fba14"
-            },
-            {
-                "action": "add",
-                "storage_identifier": "647b22ed-3eea-4a5f-9680-17f7c5e02a41"
-            }
-        ],
-        "storage_service": "ida",
-        # This project is created in ansible playbook
-        "csc_project": "system_test_project_ida"
-    },
+    "fileset": {},
     "actors": [
         {
             "roles": [
@@ -162,6 +159,53 @@ DIRS_TO_CLEAR = (
 DATA_CATALOG_PAS = "urn:nbn:fi:att:data-catalog-pas"
 
 
+@pytest.fixture(scope="function")
+def metax_client():
+    configuration = configparser.ConfigParser()
+    # TODO: /etc/siptools_research.conf might have stricter permissions
+    # in the future, so this might fail.
+    configuration.read("/etc/siptools_research.conf")
+
+    # Initialize metax client and post test dataset to Metax
+    metax_client = Metax(
+        url=configuration["siptools_research"]["metax_url"],
+        token=configuration["siptools_research"]["metax_token"],
+        verify=False,
+    )
+    return metax_client
+
+
+@pytest.fixture(scope="function")
+def tus_client():
+    """
+    tus client configured ready for uploads to upload-rest-api
+    """
+    auth_value = base64.b64encode(b"test:test").decode("utf-8")
+    tus_client = tusclient.client.TusClient(
+        f"{UPLOAD_API_URL}/files_tus",
+        headers={
+            "Authorization": f"Basic {auth_value}"
+        }
+    )
+
+    return tus_client
+
+
+@pytest.fixture(scope="function")
+def setup_upload_rest_api():
+    """Create user test:test to upload-rest-api."""
+    project_path = pathlib.Path("/var/spool/upload/projects/test_project")
+
+    # Creating test user. Project directory is created first to ensure
+    # correct directory ownership.
+    subprocess.run(
+        ["sudo", "-u", project_path.parent.owner(), 'mkdir', project_path],
+        check=True
+    )
+    Project.create(identifier="test_project")
+    User.create(username="test", projects=["test_project"], password="test")
+
+
 @pytest.fixture(scope="function", autouse=True)
 def setup_e2e():
     """Cleanup procedure executed before each E2E test."""
@@ -188,23 +232,144 @@ def setup_e2e():
         )
 
 
-def test_preservation_ida():
-    """Test the preservation workflow using IDA."""
-    # Read siptools-research config that should be installed with ansible
-    # playbook
-    configuration = configparser.ConfigParser()
-    configuration.read("/etc/siptools_research.conf")
-
-    # Initialize metax client and post test dataset to Metax
-    metax_client = Metax(
-        url=configuration["siptools_research"]["metax_url"],
-        token=configuration["siptools_research"]["metax_token"],
-        verify=False,
+def _check_uploaded_file(name, md5):
+    """Check that the uploaded file was saved with the expected metadata
+    and update its parent directory with use category"""
+    response = REQUESTS_SESSION.get(
+        f"{UPLOAD_API_URL}/files/test_project/{name}",
+        auth=("test", "test")
     )
-    dataset = metax_client.post_dataset(DATASET)
+    assert response.status_code == 200
+
+    result = response.json()
+    assert result['file_path'] == f"/{name}"
+    assert result['identifier'].startswith('urn:uuid:')
+
+
+@pytest.mark.usefixtures("setup_upload_rest_api")
+def test_preservation_local_tus(metax_client, tus_client):
+    """Test the preservation workflow using upload-rest-api TUS API."""
+    # Upload TIFF file to pre-ingest file storage
+    test_dir_path = (
+        f"e2e-test-local-tus-"
+        f"{datetime.datetime.now(datetime.timezone.utc).isoformat()}"
+    )
+    # TODO: File validation does not tolerate '+' character. Should it?
+    test_dir_path = test_dir_path.replace("+", "_")
+
+    try:
+        tus_client.uploader(
+            "tests/data/e2e_files/valid_tiff/download",
+            metadata={
+                "filename": "valid_tiff ä.tiff",
+                "project_id": "test_project",
+                "upload_path": f"{test_dir_path}/valid_tiff ä.tiff",
+                "type": "file"
+            },
+            metadata_encoding="utf-8",
+            verify_tls_cert=False
+        ).upload()
+    except tusclient.exceptions.TusCommunicationError as error:
+        logger.error(error.response_content)
+        raise
+
+    _check_uploaded_file(
+        name=f"{test_dir_path}/valid_tiff ä.tiff",
+        md5="3cf7c3b90f5a52b2f817a1c5b3bfbc52"
+    )
+
+    # Upload HTML file
+    try:
+        tus_client.uploader(
+            "tests/data/e2e_files/html_file/download",
+            metadata={
+                "filename": "html_file",
+                "project_id": "test_project",
+                "upload_path": f"{test_dir_path}/html_file",
+                "type": "file"
+            },
+            metadata_encoding="utf-8",
+            verify_tls_cert=False
+        ).upload()
+    except tusclient.exceptions.TusCommunicationError as error:
+        logger.error(error.response_content)
+        raise
+
+    _check_uploaded_file(
+        name=f"{test_dir_path}/html_file",
+        md5="31ff97b5791a2050f08f471d6205f785"
+    )
+
+    # Create dataset
+    dataset = copy.deepcopy(DATASET)
+    dataset["title"]["en"] = (
+        f"E2E test dataset - pre-ingest file storage - "
+        f"{datetime.datetime.now(datetime.timezone.utc).isoformat()}"
+    )
+
+    # Add pre-ingest files to dataset
+    dataset["data_catalog"] = "urn:nbn:fi:att:data-catalog-pas"
+    dataset["fileset"] = {
+        "directory_actions": [
+            {
+                "action": "add",
+                "pathname": f"/{test_dir_path}/"
+            }
+        ],
+        "storage_service": "pas",
+        "csc_project": "test_project"
+    }
+
+    # Submit dataset
+    dataset = metax_client.post_dataset(dataset)
     dataset_identifier = dataset["id"]
+
     logger.debug("Created dataset: %s", dataset_identifier)
 
+    _track_dataset(
+        metax_client=metax_client,
+        dataset_identifier=dataset_identifier
+    )
+
+
+def test_preservation_ida(metax_client):
+    """Test the preservation workflow using IDA."""
+    dataset = copy.deepcopy(DATASET)
+    dataset["title"]["en"] = (
+        f"E2E test dataset - IDA - "
+        f"{datetime.datetime.now(datetime.timezone.utc).isoformat()}"
+    )
+    # Add IDA files to the dataset
+    dataset["data_catalog"] = "urn:nbn:fi:att:data-catalog-ida"
+    dataset["fileset"] = {
+        # These files are added to Metax in ansible playbook
+        "file_actions": [
+            {
+                "action": "add",
+                "storage_identifier": "dfea5aa8-c9bc-4333-8eab-0b17ee6fba14"
+            },
+            {
+                "action": "add",
+                "storage_identifier": "647b22ed-3eea-4a5f-9680-17f7c5e02a41"
+            }
+        ],
+        "storage_service": "ida",
+        # This project is created in ansible playbook
+        "csc_project": "system_test_project_ida"
+    }
+
+    dataset = metax_client.post_dataset(dataset)
+    dataset_identifier = dataset["id"]
+
+    logger.debug("Created dataset: %s", dataset_identifier)
+
+    _track_dataset(
+        metax_client=metax_client,
+        dataset_identifier=dataset_identifier
+    )
+
+
+def _track_dataset(metax_client, dataset_identifier):
     try:
         logger.debug(
             "Ensure that the dataset does not have preservation state"
